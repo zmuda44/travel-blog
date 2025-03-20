7536b126d47aae831ac14480d7c252a8353b69b2
Greg Bailey <gregbailey617@gmail.com>
2025-03-20 00:07:11 -0400

    connected to model and db

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const { User } = require('../../models');
 
-router.get('/userstest', (req, res) => {
-  res.send("userstest")
+
+router.get('/', async (req, res) => {
+  try {
+    const response = await User.find({});
+
+    console.log(response)
+
+    res.send(response)
+  }
+  catch (err) {
+    res.send(err)
+  }
 }) 
 
 module.exports = router
